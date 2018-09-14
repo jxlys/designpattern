c8caed87d1d55599350f072f025bd90712e2a5cc
jxlys <wsc0703@126.com>
2018-09-14 18:51:56 +0800

    Prototype

--- src/cn/test/create/c5/PersonProtoType.java
+++ src/cn/test/create/c5/PersonProtoType.java
@@ -2,9 +2,10 @@ package cn.test.create.c5;
 
 /**
  * 原型方式
+ * 
  * @author Administrator
- *	https://www.cnblogs.com/supersheep/articles/4220669.html
+ * 
  */
 public class PersonProtoType {
-
+	String url = "https://www.cnblogs.com/supersheep/articles/4220669.html";
 }
