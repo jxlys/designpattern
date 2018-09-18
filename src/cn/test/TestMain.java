2fdd670abf9a686d26b0d839644888fead57443c
jxlys <wsc0703@126.com>
2018-09-18 11:50:37 +0800

    test

--- src/cn/test/TestMain.java
+++ src/cn/test/TestMain.java
@@ -1,9 +1,38 @@
 package cn.test;
 
-import cn.test.create.c4.PersonBuilder;
+import cn.test.create.c2.PersonFactory;
+import cn.test.entity.Person;
+import cn.test.entity.PersonType;
+import lombok.Data;
 
 public class TestMain {
 	public static void main(String[] args) {
-		new PersonBuilder().addMan();
+		Animal a = Animal.getInstance();
+		System.out.println(a);
+	}
+}
+
+@Data
+class Animal implements Cloneable {
+	private Animal() {
+		name = "1";
+		p = PersonFactory.createPerson(PersonType.BAD_USER);
+	}
+
+	private String name;
+	private Person p;
+
+	static class B {
+		private static Animal animal = new Animal();
+	}
+
+	public static Animal getInstance() {
+		try {
+			Animal b = (Animal) B.animal.clone();
+			
+			return b;
+		} catch (CloneNotSupportedException e) {
+		}
+		return null;
 	}
 }
